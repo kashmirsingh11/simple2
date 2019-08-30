6be9321ad80a26992ef6513057a652a44ed38b70
kashmirsingh11 <Arun@890>
2019-08-31 05:05:53 +0530

    Revert "fifth commit"
    This reverts commit a6ef2f0f

--- src/mydemo.java
+++ src/mydemo.java
@@ -2,6 +2,5 @@ public class mydemo {
     public static void main(String[] args) {
         System.out.println("new statement!");
         System.out.println("hello");
-        System.out.println("hanji hello");
      }
 }
