7f9d3b93db4501e27e40eeba26f227cfa11c36ac
kashmirsingh11 <Arun@890>
2019-08-31 05:10:45 +0530

    new commit!

--- src/mydemo.java
+++ src/mydemo.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class mydemo {
     public static void main(String[] args) {
         System.out.println("new statement!");
         System.out.println("hello");
+        ArrayList<Integer> list  =new ArrayList<>();
      }
 }
