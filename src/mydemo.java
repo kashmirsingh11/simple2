a6ef2f0fce5f9a8030a55d7241cb01fbfe666b5a
kashmirsingh11 <Arun@890>
2019-08-31 05:04:59 +0530

    fifth commit

--- src/mydemo.java
+++ src/mydemo.java
@@ -2,5 +2,6 @@ public class mydemo {
     public static void main(String[] args) {
         System.out.println("new statement!");
         System.out.println("hello");
+        System.out.println("hanji hello");
      }
 }
